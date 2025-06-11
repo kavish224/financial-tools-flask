b10412e4ef64863f13f396c7e286d85b935cf08c
Kavish Ambani <kavishkkambani@gmail.com>
2025-06-11 12:23:41 +0530

    add  schedule

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from app.extensions import db, migrate
 from app.routes import register_routes
 from config import Config
 from flask_cors import CORS
+from flask_apscheduler import APScheduler
+from app.services.near_sma import update_sma_results
 def create_app():
     app = Flask(__name__)
 
@@ -15,5 +17,14 @@ def create_app():
 
     # Register routes
     register_routes(app)
-
+    scheduler = APScheduler()
+    scheduler.init_app(app)
+    scheduler.start()
+    scheduler.add_job(
+        id="daily_sma_job",
+        func=lambda: update_sma_results(50, 2.0),
+        trigger="cron",
+        hour=18,
+        minute=00
+    )
     return app
